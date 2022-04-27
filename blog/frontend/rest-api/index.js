2e0ae49a640e0e9c477e5bfd924d36a61c36d0d7
Lewis Inches <incheslewis@gmail.com>
2022-04-27 21:21:52 +0100

    created REST API and connected frontend to database

---
+++ blog/frontend/rest-api/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const helmet = require("helmet")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const mongoString = 'mongodb+srv://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@90-days-blog.evwpf.mongodb.net/blog?retryWrites=true&w=majority'
+mongoose.connect(mongoString, {useNewUrlParser: true, useUnifiedTopology: true})
+
+mongoose.connection.on("error", function(error) {
+  if (process.env.NODE_ENV === "development") {
+    console.log(error)
+  }
+})
+
+mongoose.connection.on("open", function() {
+  console.log("Connected to MongoDB database.")
+})
+
+app.use(helmet())
+
+app.use(require("./routes/index.js"))
+
+app.listen(PORT, function () {
+  console.log(`Express app listening on port ${PORT}`)
+})
+
+
