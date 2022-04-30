eb94f3d90992ec38dc22a64e50c51c2adfd16586
Lewis Inches <incheslewis@gmail.com>
2022-04-30 14:49:37 +0100

    deployed website to server and started on admin section

--- blog/frontend/website/next.config.js
+++ blog/frontend/website/next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
       "DEV_API_URL": "http://localhost:5000",
-      "PRODUCTION_API_URL": "https://www.api.example.com"
+      "PRODUCTION_API_URL": "http://api.cultofdionysus.co.uk",
+      "GOOGLE_VERIFICATION_ID":"5Ya1BnEinO3wGY0q3D2n2Ead8OgdrWEBeEgxUk_RTfo"
     }
   }
