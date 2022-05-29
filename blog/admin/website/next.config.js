499bdf59818e462f7ba522cdc24be84de81ab8fe
Lewis Inches <incheslewis@gmail.com>
2022-05-29 06:08:58 +0100

    update config

--- blog/admin/website/next.config.js
+++ blog/admin/website/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
       "DEV_FRONTEND_API_URL": "http://localhost:5000",
       "DEV_ADMIN_API_URL": "http://localhost:5001",
       "PRODUCTION_ADMIN_API_URL": "http://www.admin-api.cultofdionysus.co.uk",
-      "PRODUCTION_FRONTEND_API_URL": "http://www.api.cultofdionysus.co.uk",
+      "PRODUCTION_FRONTEND_API_URL": "http://api.cultofdionysus.co.uk",
       "PRODUCTION_FRONTEND_WEBSITE_URL": "http://www.cultofdionysus.co.uk"
     }
   }
